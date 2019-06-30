991d6a9da608f8629e2e8774bcd395589af2e18c
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-06-30 17:45:54 +0530

    Update module1.groovy

--- vars/module1.groovy
+++ vars/module1.groovy
@@ -1,12 +1,21 @@
+
+
 node{
 
 	def config = new OSBCIConfig().getConfig();
-	
-	//stage('checkout')
-	//checkoutCode(config);
+	properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), parameters([string(defaultValue: '', description: '', name: 'branchName', trim: false)]), pipelineTriggers([pollSCM('* * 1 1 *')])])
+	stage('checkout')
+	checkoutCode();
 	echo "this is a string ${params.branchName}";
 }
-//def checkoutCode(config) {
+def checkoutCode(config) {
+	echo "checking out with ::::::::"
+    checkout([$class: 'GitSCM', 
+             branches: [[name: params.branchName]], 
+			 doGenerateSubmoduleConfigurations: false, 
+			 extensions: [], 
+			 submoduleCfg: [],
+			 userRemoteConfigs: [[url: 'https://github.com/manidhar1986/jenkins-test']]])
 	
-//}
+}
 
