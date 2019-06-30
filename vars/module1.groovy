42086ffed0b55fa9f8e2a71b8fe2fbf8897e3d0c
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-06-30 17:06:09 +0530

    test

--- vars/module1.groovy
+++ vars/module1.groovy
@@ -3,10 +3,10 @@ node{
 	def config = new OSBCIConfig().getConfig();
 	
 	stage('checkout')
-	checkoutCode(config);
-	echo "this is a string ${config.gitURL}";
+	//checkoutCode(config);
+	echo "this is a string ${params.branchName}";
 }
-def checkoutCode(config) {
+//def checkoutCode(config) {
 	
-}
+//}
 
