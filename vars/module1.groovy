687b2157b3bf14b354fdaf47b4679a05d46b0203
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-06-30 17:48:40 +0530

    Update module1.groovy

--- vars/module1.groovy
+++ vars/module1.groovy
@@ -3,9 +3,10 @@
 node{
 
 	def config = new OSBCIConfig().getConfig();
-	properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), parameters([string(defaultValue: '', description: '', name: 'branchName', trim: false)]), pipelineTriggers([pollSCM('* * 1 1 *')])])
-	stage('checkout')
+	//properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), parameters([string(defaultValue: '', description: '', name: 'branchName', trim: false)]), pipelineTriggers([pollSCM('* * 1 1 *')])])
+	stage('checkout'){
 	checkoutCode();
+	}
 	echo "this is a string ${params.branchName}";
 }
 def checkoutCode(config) {
