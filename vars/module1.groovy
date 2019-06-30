37257a0033a8a7b95e60bc55e7eca1699ca6867f
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-06-30 17:57:11 +0530

    Update module1.groovy

--- vars/module1.groovy
+++ vars/module1.groovy
@@ -3,7 +3,7 @@
 node{
 
 	def config = new OSBCIConfig().getConfig();
-	//properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), parameters([string(defaultValue: '', description: '', name: 'branchName', trim: false)]), pipelineTriggers([pollSCM('* * 1 1 *')])])
+	properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), parameters([string(defaultValue: '', description: '', name: 'branchName', trim: false)]), pipelineTriggers([pollSCM('* * 1 1 *')])])
 	stage('checkout'){
 	checkoutCode();
 	}
