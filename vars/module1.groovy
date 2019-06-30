b02fa6da2e50fca495aa1199fcfd79c0acb1c458
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-06-30 15:36:58 +0530

    initial changes

--- vars/module1.groovy
+++ vars/module1.groovy
@@ -2,5 +2,11 @@ node{
 
 	def config = new OSBCIConfig().getConfig();
 	
+	stage('checkout')
+	checkoutCode(config);
 	echo "this is a string ${config.gitURL}";
 }
+def checkoutCode(config) {
+	
+}
+
