020426816d6f92c6e93d4e549eb927fec8d0f32b
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-07-05 23:11:52 +0530

    Update gitTools.groovy

--- vars/gitTools.groovy
+++ vars/gitTools.groovy
@@ -1,10 +1,10 @@
 def checkOut(gitconfig) {
-	echo "checking {gitconfig}"
+	echo "checking:::::::::::::: {gitconfig}"
     checkout([$class: 'GitSCM', 
              branches: [[name: params.branchName]], 
 			 doGenerateSubmoduleConfigurations: false, 
 			 extensions: [], 
 			 submoduleCfg: [],
 userRemoteConfigs: [[
-url: gitconfig.gitURL]]])
+url: 'https://github.com/manidhar1986/jenkins-test']]])
 }
