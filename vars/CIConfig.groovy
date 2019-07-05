7294301b5ef0d99ed0df9b774528ae936bb27b7b
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-07-05 17:36:09 +0530

    Update CIConfig.groovy

--- vars/CIConfig.groovy
+++ vars/CIConfig.groovy
@@ -1,7 +1,6 @@
 def getConfig() {	
 	return [		          
-gitURL: https://github.com/manidhar1986/jenkins-test
-				
+gitURL: 'https://github.com/manidhar1986/jenkins-test'				
 		]
 	//jenkinsCredentialsId : '29c93fe8-0bd4-4599-8ae4-39a6fec70b71',
 		//svnURL: 'svn+ssh://abc@amazon.in/svn/',	
