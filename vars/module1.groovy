ddec4a32780da0f164fcf5632674bb6d2fda2e21
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-07-05 18:00:57 +0530

    Update module1.groovy

--- vars/module1.groovy
+++ vars/module1.groovy
@@ -3,7 +3,7 @@ node{
 	def config = new CIConfig().getConfig();
 	properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), parameters([string(defaultValue: '', description: '', name: 'branchName', trim: false)]), pipelineTriggers([pollSCM('* * 1 1 *')])])
 	stage('checkout'){
-		echo "params:::::::::::::::::: ${params.config}";
+		echo "params:::::::::::::::::: ${config}";
 		echo "params-------------------->>>> ${params}";
 	checkoutCode(config);
 	}
