1cf733506892481291ab78436d14e23d48e28d6c
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-07-05 23:31:19 +0530

    Update module1.groovy

--- vars/module1.groovy
+++ vars/module1.groovy
@@ -1,5 +1,4 @@
 node{
-
 	def config = new CIConfig().getConfig();
 	properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), parameters([string(defaultValue: '', description: '', name: 'branchName', trim: false)]), pipelineTriggers([pollSCM('* * 1 1 *')])])
 	stage('checkout'){
@@ -11,5 +10,7 @@ node{
 }
 def checkoutCode(config) {
 	echo "In module1.groovy"
-	gitTools.checkOut(URL: "${config.gitURL}")
+	echo "${config.gitURL}"
+	echo"hi"
+	gitTools.checkOut(URL: "${config.gitURL}");
 }
