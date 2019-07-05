e72cb5201956878cc9717cf325d82483adf073ee
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-07-06 01:06:11 +0530

    Update CIConfig.groovy

--- vars/CIConfig.groovy
+++ vars/CIConfig.groovy
@@ -1,6 +1,7 @@
 def getConfig() {	
 	return [		          
-gitURL: 'https://github.com/manidhar1986/jenkins-test'
+gitURL: 'https://github.com/manidhar1986/jenkins-test',
+jfrogURL: 'https://myjfrogtest.jfrog.io/myjfrogtest/'
 ]
 }
 
