3e154b21a53b4a0b9194f87a17b287bb9c487e59
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-07-05 23:56:51 +0530

    Update module1.groovy

--- vars/module1.groovy
+++ vars/module1.groovy
@@ -1,16 +1,42 @@
 node{
 	def config = new CIConfig().getConfig();
+	try {
 	properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), parameters([string(defaultValue: '', description: '', name: 'branchName', trim: false)]), pipelineTriggers([pollSCM('* * 1 1 *')])])
 	stage('checkout'){
-		echo "params:::::::::::::::::: ${config}";
-		echo "params-------------------->>>> ${params}";
-	checkoutCode(config);
+	checkoutCode();
 	}
+	stage('upload'){
+	uploadCode();
+	}
+	echo "this is a string ${params.branchName}";
+	}catch (Exception e){
+			 echo "Failure occured at some stage in pipeline with ${params.branchName}";
+			//sendEmailNotification(config: config,statusJob: statusJob)
+			throw e;
+		}
+	
 	
 }
-def checkoutCode(config) {
-	echo "In module1.groov1111y"
+def checkoutCode() {
+	echo "checking out with ::::::::"
+    checkout([$class: 'GitSCM', 
+             branches: [[name: params.branchName]], 
+			 doGenerateSubmoduleConfigurations: false, 
+			 extensions: [], 
+			 submoduleCfg: [],
+			 userRemoteConfigs: [[url: 'https://github.com/manidhar1986/jenkins-test']]])
 	
-	echo"hi"
-	gitTools.checkOut(URL: "${config.gitURL}")
+}
+
+def uploadCode() {
+	def server = Artifactory.newServer url: 'https://myjfrogtest.jfrog.io/myjfrogtest/', username: 'venkat', password: 'Test@123'
+	def uploadSpec = """{
+      "files": [
+        {
+          "pattern": "vars/*",
+	  "target": "jenkins-integration"
+        }
+     ]
+    }"""	
+		server.upload spec: uploadSpec
 }
