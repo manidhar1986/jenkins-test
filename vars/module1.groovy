ad8f27d14a68c39170718492f901f918d2139d49
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-07-05 17:37:38 +0530

    Update module1.groovy

--- vars/module1.groovy
+++ vars/module1.groovy
@@ -3,10 +3,11 @@ node{
 	def config = new CIConfig().getConfig();
 	properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), parameters([string(defaultValue: '', description: '', name: 'branchName', trim: false)]), pipelineTriggers([pollSCM('* * 1 1 *')])])
 	stage('checkout'){
+		echo "this is a string ${params}";
 	checkoutCode(config);
 	}
-	echo "this is a string ${params.branchName}";
+	
 }
 def checkoutCode(config) {
-	gitTools.checkOut(URL: "${config.gitURL}", jenkinsCredentialsID: "${config.jenkinsCredentialsID}")
+	gitTools.checkOut(URL: "${config.gitURL}")
 }
