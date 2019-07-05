0df4710aa115d932eaff0e528d3a51d922e25579
manidhar1986 <31403663+manidhar1986@users.noreply.github.com>
2019-07-05 17:34:25 +0530

    Update CIConfig.groovy

--- vars/CIConfig.groovy
+++ vars/CIConfig.groovy
@@ -6,7 +6,7 @@ gitURL: https://github.com/manidhar1986/jenkins-test
 	//jenkinsCredentialsId : '29c93fe8-0bd4-4599-8ae4-39a6fec70b71',
 		//svnURL: 'svn+ssh://abc@amazon.in/svn/',	
 //ab_repo: 'http://xxxxxx:8081/nexus/content/repositories/xxxxx/',
-//ab_repo_id: 'den01cee',
+//ab_repo_id: 'abc',
 }
 
 
